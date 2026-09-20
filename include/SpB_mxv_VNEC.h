8af6a4b9b45d52f02bc4eb5bcb622045598d9c7e
zagriadskov maksim <maksimzagradskrv50910@gmail.com>
2026-09-20 14:11:52 +0300

    add license

--- include/SpB_mxv_VNEC.h
+++ include/SpB_mxv_VNEC.h
@@ -1,3 +1,16 @@
+/* 
+*========================================================
+ * Copyright (c) RVVLASparse and Lobachevsky State University of 
+ * Nizhny Novgorod and its affiliates. All rights reserved.
+ * 
+ * Copyright 2026 The RVVLASparse Authors (Zagriadskov Maksim)
+ *
+ * Distributed under the MIT License
+ * (See file LICENSE in the root directory of this 
+ * source tree)
+ *========================================================
+ */
+
 #pragma once
 #include <assert.h>
 #include <string.h>
