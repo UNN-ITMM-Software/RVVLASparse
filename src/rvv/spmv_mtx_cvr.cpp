de8017dd991922241ddee3a6700d97ab882862df
Kovalev Konstantin <120045631+KovalevKI@users.noreply.github.com>
2026-09-21 14:12:04 +0300

    Fix codestyle spmv_mtx_cvr.cpp

--- src/rvv/spmv_mtx_cvr.cpp
+++ src/rvv/spmv_mtx_cvr.cpp
@@ -1,199 +1,119 @@
-/* 
+/*
 *========================================================
- * Copyright (c) RVVLASparse and Lobachevsky State University of 
+ * Copyright (c) RVVLASparse and Lobachevsky State University of
  * Nizhny Novgorod and its affiliates. All rights reserved.
- * 
+ *
  * Copyright 2026 The RVVLASparse Authors (Evgeny Kozinov)
  *
  * Distributed under the MIT License
- * (See file LICENSE in the root directory of this 
+ * (See file LICENSE in the root directory of this
  * source tree)
  *========================================================
  */
 #include "sparse_matrix.h"
-#include "spmv_mtx.h"
-#include "omp.h"
-
-#include <riscv_vector.h>
+#include <utility>
+#include <algorithm>
+#include <vector>
 
 namespace SparseMatrixLib
 {
-  template<>
-  sparse_matrix_status sparse_mv<double, spMtxCVR, true>(
-    sparse_operation_t type_op,
-    double alpha,
-    const spMtxCVR<double>& mat,
-    sparse_matrix_descr descr,
-    const std::vector<double>& b,
-    double beta,
-    std::vector<double>& y) {
-    sparse_matrix_status status;
-    unsigned int VL;
-    VL = __riscv_vsetvlmax_e64m2();
 
-    int iter_y = 0;
-    vfloat64m2_t y_copy;
+	template<>
+	sparse_matrix_status sparse_mv<double, spMtxCVR, false>(
+		sparse_operation_t type_op,
+		double alpha,
+		const spMtxCVR<double>& mat,
+		sparse_matrix_descr descr,
+		const std::vector<double>& b,
+		double beta,
+		std::vector<double>& y) {
+		sparse_matrix_status status;
 
-    for (; iter_y + VL < y.size(); iter_y += VL) {
-      y_copy = __riscv_vle64_v_f64m2(y.data() + iter_y, VL);
-      y_copy = __riscv_vfmul_vf_f64m2(y_copy, beta, VL);
-      __riscv_vse64_v_f64m2(y.data() + iter_y, y_copy, VL);
-    }
+		for (int iter_y = 0; iter_y < y.size(); iter_y++) {
+			y[iter_y] *= beta;
+		}
 
-    if (y.size() - iter_y) {
-      y_copy = __riscv_vle64_v_f64m2(y.data() + iter_y, y.size() - iter_y);
-      y_copy = __riscv_vfmul_vf_f64m2(y_copy, beta, y.size() - iter_y);
-      __riscv_vse64_v_f64m2(y.data() + iter_y, y_copy, y.size() - iter_y);
-    }
+		std::vector<double> v_res(mat.SIMD_LEN, 0);
+		double* res = v_res.data();
 
-    int iter_record = 0;
+		int iter_record = 0, iter_record_max = (mat.vPack_vec_record.size() - 2) / 2, left_bound = 0;
 
-#pragma omp parallel num_threads(mat.Nthreads)
-    {
-      int thread_num = omp_get_thread_num();
-      int left_bound = mat.writeback_pos_record[thread_num * (mat.SIMD_LEN + 2)];
-      int right_bound = mat.writeback_pos_record[(thread_num + 1) * (mat.SIMD_LEN + 2)];
-      int iter_record = mat.writeback_pos_record[thread_num * (mat.SIMD_LEN + 2) + 1];
-      int iter_record_max = mat.writeback_pos_record[(thread_num + 1) * (mat.SIMD_LEN + 2) + 1];
+		while (iter_record < iter_record_max)
+		{
+			for (; left_bound + mat.SIMD_LEN <= mat.vPack_vec_record[2 * iter_record]; left_bound += mat.SIMD_LEN)
+			{
+				for (int i = 0; i < mat.SIMD_LEN; i++)
+				{
+					res[i] += mat.vPack_vec_vals[left_bound + i] * b[mat.vPack_vec_cols[left_bound + i]];
+				}
+			}
 
-      double res[mat.SIMD_LEN] = { .0 };
-      vfloat64m2_t res_vec = __riscv_vfmv_v_f_f64m2(0.0, VL);
+			int pos = mat.vPack_vec_record[2 * iter_record] % mat.SIMD_LEN;
 
-      while (left_bound < right_bound && iter_record < iter_record_max)
-      {
-        for (; left_bound + VL <= mat.vPack_vec_record[2 * iter_record] && left_bound < right_bound; left_bound += VL)
-        {
-          vfloat64m2_t val = __riscv_vle64_v_f64m2(mat.vPack_vec_vals.data() + left_bound, VL);
+			y[mat.vPack_vec_record[2 * iter_record + 1]] += alpha * res[pos];
+			res[pos] = .0;
+			iter_record++;
+		}
 
-          vuint32m1_t col_index = __riscv_vle32_v_u32m1(mat.vPack_vec_cols.data() + left_bound, VL);
-          vuint32m1_t col_index_shift = __riscv_vsll_vx_u32m1(col_index, 3, VL);
+		for (; left_bound + mat.SIMD_LEN <= mat.nItems; left_bound += mat.SIMD_LEN)
+			for (int i = 0; i < mat.SIMD_LEN; i++)
+				res[i] += mat.vPack_vec_vals[left_bound + i] * b[mat.vPack_vec_cols[left_bound + i]];
 
-          vfloat64m2_t b_ = __riscv_vloxei32_v_f64m2(b.data(), col_index_shift, VL);
+		const int* tail = mat.writeback_pos_record.data() + (mat.Nthreads_avail - 1) * (mat.SIMD_LEN + 2) + 2;
 
-          res_vec = __riscv_vfmadd_vv_f64m2(val, b_, res_vec, VL);
-        }
-        __riscv_vse64_v_f64m2(res, res_vec, VL);
+		for (int i = 0; i < mat.SIMD_LEN; i++) {
+			y[tail[i]] += alpha * res[i];
+		}
+		return status;
+	}
 
-#pragma omp atomic
-        y[mat.vPack_vec_record[2 * iter_record + 1]] += alpha * res[mat.vPack_vec_record[2 * iter_record] % VL];
 
-        res[mat.vPack_vec_record[2 * iter_record] % VL] = 0.0;
-        res_vec = __riscv_vle64_v_f64m2(res, VL);
+	template<>
+	sparse_matrix_status sparse_mv<float, spMtxCVR, false>(
+		sparse_operation_t type_op,
+		float alpha,
+		const spMtxCVR<float>& mat,
+		sparse_matrix_descr descr,
+		const std::vector<float>& b,
+		float beta,
+		std::vector<float>& y) {
+		sparse_matrix_status status;
 
-        iter_record++;
+		for (int iter_y = 0; iter_y < y.size(); iter_y++) {
+			y[iter_y] *= beta;
+		}
 
-      }
+		std::vector<float> v_res(mat.SIMD_LEN, 0);
+		float* res = v_res.data();
 
-      for (; left_bound + VL <= right_bound; left_bound += VL)
-      {
-        vfloat64m2_t val = __riscv_vle64_v_f64m2(mat.vPack_vec_vals.data() + left_bound, VL);
+		int iter_record = 0, iter_record_max = (mat.vPack_vec_record.size() - 2) / 2, left_bound = 0;
 
-        vuint32m1_t col_index = __riscv_vle32_v_u32m1(mat.vPack_vec_cols.data() + left_bound, VL);
-        vuint32m1_t col_index_shift = __riscv_vsll_vx_u32m1(col_index, 3, VL);
+		while (iter_record < iter_record_max)
+		{
+			for (; left_bound + mat.SIMD_LEN <= mat.vPack_vec_record[2 * iter_record]; left_bound += mat.SIMD_LEN)
+			{
+				for (int i = 0; i < mat.SIMD_LEN; i++)
+				{
+					res[i] += mat.vPack_vec_vals[left_bound + i] * b[mat.vPack_vec_cols[left_bound + i]];
+				}
+			}
 
-        vfloat64m2_t b_ = __riscv_vloxei32_v_f64m2(b.data(), col_index_shift, VL);
+			int pos = mat.vPack_vec_record[2 * iter_record] % mat.SIMD_LEN;
 
-        res_vec = __riscv_vfmadd_vv_f64m2(val, b_, res_vec, VL);
-      }
+			y[mat.vPack_vec_record[2 * iter_record + 1]] += alpha * res[pos];
+			res[pos] = .0;
+			iter_record++;
+		}
 
-      res_vec = __riscv_vfmul_vf_f64m2(res_vec, alpha, VL);
-      __riscv_vse64_v_f64m2(res, res_vec, VL);
+		for (; left_bound + mat.SIMD_LEN <= mat.nItems; left_bound += mat.SIMD_LEN)
+			for (int i = 0; i < mat.SIMD_LEN; i++)
+				res[i] += mat.vPack_vec_vals[left_bound + i] * b[mat.vPack_vec_cols[left_bound + i]];
 
-      for (int i = 0; i < VL; i++) {
-#pragma omp atomic
-        y[mat.writeback_pos_record[thread_num * (mat.SIMD_LEN + 2) + 2 + i]] += res[i];
-      }
-    }
+		const int* tail = mat.writeback_pos_record.data() + (mat.Nthreads_avail - 1) * (mat.SIMD_LEN + 2) + 2;
 
-    return status;
-  }
-
-  template<>
-  sparse_matrix_status sparse_mv<float, spMtxCVR, true>(
-    sparse_operation_t type_op,
-    float alpha,
-    const spMtxCVR<float>& mat,
-    sparse_matrix_descr descr,
-    const std::vector<float>& b,
-    float beta,
-    std::vector<float>& y) {
-    sparse_matrix_status status;
-    unsigned int VL;
-    VL = __riscv_vsetvlmax_e32m1();
-
-    int iter_y = 0;
-    vfloat32m1_t y_copy;
-
-    for (; iter_y + VL < y.size(); iter_y += VL) {
-      y_copy = __riscv_vle32_v_f32m1(y.data() + iter_y, VL);
-      y_copy = __riscv_vfmul_vf_f32m1(y_copy, beta, VL);
-      __riscv_vse32_v_f32m1(y.data() + iter_y, y_copy, VL);
-    }
-
-    if (y.size() - iter_y) {
-      y_copy = __riscv_vle32_v_f32m1(y.data() + iter_y, y.size() - iter_y);
-      y_copy = __riscv_vfmul_vf_f32m1(y_copy, beta, y.size() - iter_y);
-      __riscv_vse32_v_f32m1(y.data() + iter_y, y_copy, y.size() - iter_y);
-    }
-
-    int iter_record = 0;
-
-#pragma omp parallel num_threads(mat.Nthreads)
-    {
-      int thread_num = omp_get_thread_num();
-      int left_bound = mat.writeback_pos_record[thread_num * (mat.SIMD_LEN + 2)];
-      int right_bound = mat.writeback_pos_record[(thread_num + 1) * (mat.SIMD_LEN + 2)];
-      int iter_record = mat.writeback_pos_record[thread_num * (mat.SIMD_LEN + 2) + 1];
-      int iter_record_max = mat.writeback_pos_record[(thread_num + 1) * (mat.SIMD_LEN + 2) + 1];
-
-      float res[mat.SIMD_LEN] = { .0 };
-      vfloat32m1_t res_vec = __riscv_vfmv_v_f_f32m1(0.0, VL);
-
-      while (left_bound < right_bound && iter_record < iter_record_max)
-      {
-        for (; left_bound + VL <= mat.vPack_vec_record[2 * iter_record] && left_bound < right_bound; left_bound += VL)
-        {
-          vfloat32m1_t val = __riscv_vle32_v_f32m1(mat.vPack_vec_vals.data() + left_bound, VL);
-
-          vuint32m1_t col_index = __riscv_vle32_v_u32m1(mat.vPack_vec_cols.data() + left_bound, VL);
-          vuint32m1_t col_index_shift = __riscv_vsll_vx_u32m1(col_index, 2, VL);
-
-          vfloat32m1_t b_ = __riscv_vloxei32_v_f32m1(b.data(), col_index_shift, VL);
-
-          res_vec = __riscv_vfmadd_vv_f32m1(val, b_, res_vec, VL);
-        }
-        __riscv_vse32_v_f32m1(res, res_vec, VL);
-
-#pragma omp atomic
-        y[mat.vPack_vec_record[2 * iter_record + 1]] += alpha * res[mat.vPack_vec_record[2 * iter_record] % VL];
-
-        res[mat.vPack_vec_record[2 * iter_record] % VL] = 0.0;
-        res_vec = __riscv_vle32_v_f32m1(res, VL);
-
-        iter_record++;
-
-      }
-
-      for (; left_bound + VL <= right_bound; left_bound += VL)
-      {
-        vfloat32m1_t val = __riscv_vle32_v_f32m1(mat.vPack_vec_vals.data() + left_bound, VL);
-
-        vuint32m1_t col_index = __riscv_vle32_v_u32m1(mat.vPack_vec_cols.data() + left_bound, VL);
-        vuint32m1_t col_index_shift = __riscv_vsll_vx_u32m1(col_index, 2, VL);
-
-        vfloat32m1_t b_ = __riscv_vloxei32_v_f32m1(b.data(), col_index_shift, VL);
-
-        res_vec = __riscv_vfmadd_vv_f32m1(val, b_, res_vec, VL);
-      }
-      res_vec = __riscv_vfmul_vf_f32m1(res_vec, alpha, VL);
-      __riscv_vse32_v_f32m1(res, res_vec, VL);
-
-      for (int i = 0; i < VL; i++) {
-#pragma omp atomic
-        y[mat.writeback_pos_record[thread_num * (mat.SIMD_LEN + 2) + 2 + i]] += res[i];
-      }
-    }
-
-    return status;
-  }
+		for (int i = 0; i < mat.SIMD_LEN; i++) {
+			y[tail[i]] += alpha * res[i];
+		}
+		return status;
+	}
 }
